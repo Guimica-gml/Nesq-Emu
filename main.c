8635394e064be98f1522a4f39b5390576beab779
nic <nicholasalmeida567@gmail.com>
2023-12-28 20:51:06 -0300

    Trying to figure out how the architecture should look like

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <errno.h>
 
 #define ARENA_IMPLEMENTATION
@@ -11,7 +12,105 @@
 
 #define array_len(arr) sizeof(arr) / sizeof((arr)[0])
 
-typedef unsigned char byte;
+typedef uint8_t byte;
+typedef uint16_t u16;
+
+const size_t instruction_modes[256] = {
+    6, 7, 6, 7, 11, 11, 11, 11, 6, 5, 4, 5, 1, 1, 1, 1,
+    10, 9, 6, 9, 12, 12, 12, 12, 6, 3, 6, 3, 2, 2, 2, 2,
+    1, 7, 6, 7, 11, 11, 11, 11, 6, 5, 4, 5, 1, 1, 1, 1,
+    10, 9, 6, 9, 12, 12, 12, 12, 6, 3, 6, 3, 2, 2, 2, 2,
+    6, 7, 6, 7, 11, 11, 11, 11, 6, 5, 4, 5, 1, 1, 1, 1,
+    10, 9, 6, 9, 12, 12, 12, 12, 6, 3, 6, 3, 2, 2, 2, 2,
+    6, 7, 6, 7, 11, 11, 11, 11, 6, 5, 4, 5, 8, 1, 1, 1,
+    10, 9, 6, 9, 12, 12, 12, 12, 6, 3, 6, 3, 2, 2, 2, 2,
+    5, 7, 5, 7, 11, 11, 11, 11, 6, 5, 6, 5, 1, 1, 1, 1,
+    10, 9, 6, 9, 12, 12, 13, 13, 6, 3, 6, 3, 2, 2, 3, 3,
+    5, 7, 5, 7, 11, 11, 11, 11, 6, 5, 6, 5, 1, 1, 1, 1,
+    10, 9, 6, 9, 12, 12, 13, 13, 6, 3, 6, 3, 2, 2, 3, 3,
+    5, 7, 5, 7, 11, 11, 11, 11, 6, 5, 6, 5, 1, 1, 1, 1,
+    10, 9, 6, 9, 12, 12, 12, 12, 6, 3, 6, 3, 2, 2, 2, 2,
+    5, 7, 5, 7, 11, 11, 11, 11, 6, 5, 6, 5, 1, 1, 1, 1,
+    10, 9, 6, 9, 12, 12, 12, 12, 6, 3, 6, 3, 2, 2, 2, 2,
+};
+
+const size_t instruction_sizes[256] = {
+    2, 2, 0, 0, 2, 2, 2, 0, 1, 2, 1, 0, 3, 3, 3, 0,
+    2, 2, 0, 0, 2, 2, 2, 0, 1, 3, 1, 0, 3, 3, 3, 0,
+    3, 2, 0, 0, 2, 2, 2, 0, 1, 2, 1, 0, 3, 3, 3, 0,
+    2, 2, 0, 0, 2, 2, 2, 0, 1, 3, 1, 0, 3, 3, 3, 0,
+    1, 2, 0, 0, 2, 2, 2, 0, 1, 2, 1, 0, 3, 3, 3, 0,
+    2, 2, 0, 0, 2, 2, 2, 0, 1, 3, 1, 0, 3, 3, 3, 0,
+    1, 2, 0, 0, 2, 2, 2, 0, 1, 2, 1, 0, 3, 3, 3, 0,
+    2, 2, 0, 0, 2, 2, 2, 0, 1, 3, 1, 0, 3, 3, 3, 0,
+    2, 2, 0, 0, 2, 2, 2, 0, 1, 0, 1, 0, 3, 3, 3, 0,
+    2, 2, 0, 0, 2, 2, 2, 0, 1, 3, 1, 0, 0, 3, 0, 0,
+    2, 2, 2, 0, 2, 2, 2, 0, 1, 2, 1, 0, 3, 3, 3, 0,
+    2, 2, 0, 0, 2, 2, 2, 0, 1, 3, 1, 0, 3, 3, 3, 0,
+    2, 2, 0, 0, 2, 2, 2, 0, 1, 2, 1, 0, 3, 3, 3, 0,
+    2, 2, 0, 0, 2, 2, 2, 0, 1, 3, 1, 0, 3, 3, 3, 0,
+    2, 2, 0, 0, 2, 2, 2, 0, 1, 2, 1, 0, 3, 3, 3, 0,
+    2, 2, 0, 0, 2, 2, 2, 0, 1, 3, 1, 0, 3, 3, 3, 0,
+};
+PN
+const byte instruction_cycles[256] = {
+    7, 6, 2, 8, 3, 3, 5, 5, 3, 2, 2, 2, 4, 4, 6, 6,
+    2, 5, 2, 8, 4, 4, 6, 6, 2, 4, 2, 7, 4, 4, 7, 7,
+    6, 6, 2, 8, 3, 3, 5, 5, 4, 2, 2, 2, 4, 4, 6, 6,
+    2, 5, 2, 8, 4, 4, 6, 6, 2, 4, 2, 7, 4, 4, 7, 7,
+    6, 6, 2, 8, 3, 3, 5, 5, 3, 2, 2, 2, 3, 4, 6, 6,
+    2, 5, 2, 8, 4, 4, 6, 6, 2, 4, 2, 7, 4, 4, 7, 7,
+    6, 6, 2, 8, 3, 3, 5, 5, 4, 2, 2, 2, 5, 4, 6, 6,
+    2, 5, 2, 8, 4, 4, 6, 6, 2, 4, 2, 7, 4, 4, 7, 7,
+    2, 6, 2, 6, 3, 3, 3, 3, 2, 2, 2, 2, 4, 4, 4, 4,
+    2, 6, 2, 6, 4, 4, 4, 4, 2, 5, 2, 5, 5, 5, 5, 5,
+    2, 6, 2, 6, 3, 3, 3, 3, 2, 2, 2, 2, 4, 4, 4, 4,
+    2, 5, 2, 5, 4, 4, 4, 4, 2, 4, 2, 4, 4, 4, 4, 4,
+    2, 6, 2, 8, 3, 3, 5, 5, 2, 2, 2, 2, 4, 4, 6, 6,
+    2, 5, 2, 8, 4, 4, 6, 6, 2, 4, 2, 7, 4, 4, 7, 7,
+    2, 6, 2, 8, 3, 3, 5, 5, 2, 2, 2, 2, 4, 4, 6, 6,
+    2, 5, 2, 8, 4, 4, 6, 6, 2, 4, 2, 7, 4, 4, 7, 7,
+};
+
+// NOTE(nic): indicates the number of cycles used by each
+// instruction when a page is crossed, this may not be necessary
+const byte instruction_page_cycles[256] = {
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0,
+};
+
+const char *instruction_names[256] = {
+    "BRK", "ORA", "JAM", "SLO", "NOP", "ORA", "ASL", "SLO", "PHP", "ORA", "ASL", "ANC", "NOP", "ORA", "ASL", "SLO",
+    "BPL", "ORA", "JAM", "SLO", "NOP", "ORA", "ASL", "SLO", "CLC", "ORA", "NOP", "SLO", "NOP", "ORA", "ASL", "SLO",
+    "JSR", "AND", "JAM", "RLA", "BIT", "AND", "ROL", "RLA", "PLP", "AND", "ROL", "ANC", "BIT", "AND", "ROL", "RLA",
+    "BMI", "AND", "JAM", "RLA", "NOP", "AND", "ROL", "RLA", "SEC", "AND", "NOP", "RLA", "NOP", "AND", "ROL", "RLA",
+    "RTI", "EOR", "JAM", "SRE", "NOP", "EOR", "LSR", "SRE", "PHA", "EOR", "LSR", "ALR", "JMP", "EOR", "LSR", "SRE",
+    "BVC", "EOR", "JAM", "SRE", "NOP", "EOR", "LSR", "SRE", "CLI", "EOR", "NOP", "SRE", "NOP", "EOR", "LSR", "SRE",
+    "RTS", "ADC", "JAM", "RRA", "NOP", "ADC", "ROR", "RRA", "PLA", "ADC", "ROR", "ARR", "JMP", "ADC", "ROR", "RRA",
+    "BVS", "ADC", "JAM", "RRA", "NOP", "ADC", "ROR", "RRA", "SEI", "ADC", "NOP", "RRA", "NOP", "ADC", "ROR", "RRA",
+    "NOP", "STA", "NOP", "SAX", "STY", "STA", "STX", "SAX", "DEY", "NOP", "TXA", "XAA", "STY", "STA", "STX", "SAX",
+    "BCC", "STA", "JAM", "AHX", "STY", "STA", "STX", "SAX", "TYA", "STA", "TXS", "TAS", "SHY", "STA", "SHX", "AHX",
+    "LDY", "LDA", "LDX", "LAX", "LDY", "LDA", "LDX", "LAX", "TAY", "LDA", "TAX", "LAX", "LDY", "LDA", "LDX", "LAX",
+    "BCS", "LDA", "JAM", "LAX", "LDY", "LDA", "LDX", "LAX", "CLV", "LDA", "TSX", "LAS", "LDY", "LDA", "LDX", "LAX",
+    "CPY", "CMP", "NOP", "DCP", "CPY", "CMP", "DEC", "DCP", "INY", "CMP", "DEX", "AXS", "CPY", "CMP", "DEC", "DCP",
+    "BNE", "CMP", "JAM", "DCP", "NOP", "CMP", "DEC", "DCP", "CLD", "CMP", "NOP", "DCP", "NOP", "CMP", "DEC", "DCP",
+    "CPX", "SBC", "NOP", "ISC", "CPX", "SBC", "INC", "ISC", "INX", "SBC", "NOP", "SBC", "CPX", "SBC", "INC", "ISC",
+    "BEQ", "SBC", "JAM", "ISC", "NOP", "SBC", "INC", "ISC", "SED", "SBC", "NOP", "ISC", "NOP", "SBC", "INC", "ISC",
+};
 
 typedef struct {
     byte *prg;
@@ -22,13 +121,59 @@ typedef struct {
     bool battery;
 } Cartridge;
 
+#define CPU_MEMORY_CAP       0x800
+
+#define CPU_STACK_OFFSET     0x100
+#define CPU_ZERO_PAGE_OFFSET 0x000
+
+typedef enum {
+    ADDR_MODE_UNDEFINED, // Error
+    ADDR_MODE_ZERO_PAGE_X,
+    ADDR_MODE_ZERO_PAGE_Y,
+    ADDR_MODE_ABSOULUTE_X,
+    ADDR_MODE_ABSOULUTE_Y,
+    ADDR_MODE_INDIRECT_X,
+    ADDR_MODE_INDIRECT_Y,
+    ADDR_MODE_IMPLICIT,
+    ADDR_MODE_ACCUMULATOR,
+    ADDR_MODE_IMMEDIATE,
+    ADDR_MODE_ZERO_PAGE,
+    ADDR_MODE_ABSOLUTE,
+    ADDR_MODE_RELATIVE,
+    ADDR_MODE_INDIRECT,
+} Addressing_Mode;
+
 typedef struct {
+    u16  reg_pc; // Program counter
+    byte reg_sp; // Stack pointer
+    byte reg_a;  // Accumulator
+    byte reg_x;
+    byte reg_y;
+    byte reg_p;  // Processor status
+    Addressing_Mode addr_mode;
+    byte mem[CPU_MEMORY_CAP];
+} CPU;
+
+typedef struct {
+    int dummy; // TODO
+} PPU;
+
+typedef struct {
+    int dummy; // TODO
+} Controller;
+
+typedef struct {
+    CPU cpu;
+    PPU ppu;
+    Cartridge cartridge;
+    Controller controller_one;
+    Controller controller_two;
 } NES;
 
 void read_bytes(void *buffer, size_t count, FILE *stream) {
     size_t read = fread(buffer, sizeof(char), count, stream);
     if (read != count) {
-        fprintf(stderr, "Error: cound not read %zu bytes from buffer\n", count);
+        fprintf(stderr, "Error: cound not read %zu byte(s) from buffer\n", count);
         exit(1);
     }
 }
@@ -56,8 +201,8 @@ Cartridge cartridge_from_ines_file(const char *filepath, Arena *arena) {
         exit(1);
     }
 
-    byte prg_rom_size = read_byte(file);
-    byte chr_rom_size = read_byte(file);
+    byte prg_units = read_byte(file);
+    byte chr_units = read_byte(file);
 
     byte flag_06 = read_byte(file);
     byte mirroring_arrangement = flag_06 & 0x01;
@@ -104,15 +249,15 @@ Cartridge cartridge_from_ines_file(const char *filepath, Arena *arena) {
         read_bytes(&trainer, trainer_size, file);
     }
 
-    size_t prg_size = prg_rom_size * 16384;
-    size_t chr_size = chr_rom_size * 8192;
+    size_t prg_size = prg_units * 16384;
+    size_t chr_size = chr_units * 8192;
 
-    byte *prg_rom = arena_alloc(arena, prg_size);
-    byte *chr_rom = arena_alloc(arena, chr_size);
+    byte *prg = arena_alloc(arena, prg_size);
+    byte *chr = arena_alloc(arena, chr_size);
     byte *sram = arena_alloc(arena, 8192);
 
-    read_bytes(prg_rom, prg_size, file);
-    read_bytes(chr_rom, chr_size, file);
+    read_bytes(prg, prg_size, file);
+    read_bytes(chr, chr_size, file);
 
     fclose(file);
 
@@ -120,8 +265,8 @@ Cartridge cartridge_from_ines_file(const char *filepath, Arena *arena) {
     byte mirroring = mirroring_arrangement | four_screen_vram;
 
     Cartridge c = { 0 };
-    c.prg = prg_rom;
-    c.chr = chr_rom;
+    c.prg = prg;
+    c.chr = chr;
     c.sram = sram;
     c.mapper = mapper;
     c.mirror = mirroring;
@@ -129,11 +274,19 @@ Cartridge cartridge_from_ines_file(const char *filepath, Arena *arena) {
     return c;
 }
 
+NES nes_new(Cartridge cartridge) {
+    NES nes = { 0 };
+    nes.cartridge = cartridge;
+    return nes;
+}
+
 int main(void) {
     Arena arena = { 0 };
-    Cartridge cartridge = cartridge_from_ines_file(FILEPATH, &arena);
 
-    // What now?
+    Cartridge cartridge = cartridge_from_ines_file(FILEPATH, &arena);
+    NES nes = nes_new(cartridge);
+
+    (void) nes;
 
     arena_free(&arena);
     return 0;
